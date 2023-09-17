c73235afa4babc3221e761a68a5d90890ad4245a
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-09-17 14:26:39 +0330

    developed trade rules and trade challenge sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,10 @@ import localFont from 'next/font/local'
 import ArrowComponent from '@/components/ArrowComponent/ArrowComponent';
 import HeroSectionText from '@/components/HeroSectionText/HeroSectionText';
 import dynamic from 'next/dynamic';
+import { Accordion, AccordionTab } from 'primereact/accordion';
 const myFont = localFont({ src: '../assets/fonts/Mj Dinar Two Medium.ttf' })
 const myFontIran = localFont({ src: '../assets/fonts/iranyekanwebregular_0.ttf' })
+import rulesPic from '../assets/images/rules.png'
 const CarouselSlider = dynamic(() => import("@/components/CarouselSlider/CarouselSlider"), {
   ssr: false,
 });
@@ -108,7 +110,131 @@ export default function Home() {
             <Image src={bull} alt='bull' className='absolute top-1/2' unoptimized />
           </div>
 
+          <div className='py-40 text-center tradeRules'>
+            <p className={`${myFont.className} text-white text-5xl py-12`}>
+              <span className='text-3xl text-main-orange'> </span> قوانین <span style={{ color: '#F68D2E' }}> ترید </span>
+            </p>
+            <Image src={rulesPic} alt='rulesPic' unoptimized className='mx-auto' />
+            <p className={`${myFontIran.className} text-main-orange text-center`}
+              style={{ textDecoration: 'underline', transform: 'translateY(-50px)' }}>
+              مشاهده تمام قوانین
+            </p>
+          </div>
+
+          <div className='tradeChallenges'>
+            <div className='tradeChallenges__title flex flex-row-reverse items-baseline text-center justify-center gap-3'>
+              <p className={`${myFont.className} text-white text-5xl text-center`}>
+                <span className='text-3xl text-main-orange'> </span>سوالات متداول <span style={{ color: '#F68D2E' }}> چالش های تریدینگ </span>
+              </p>
+              <p className={`${myFontIran.className} text-main-orange text-center`}
+                style={{ textDecoration: 'underline' }}>
+                مشاهده تمام سوالات
+              </p>
+            </div>
+
+            <div>
+              <Accordion multiple className='flex flex-col gap-y-4 mx-12 mb-24 mt-8'>
+                <AccordionTab
+                  style={{ marginLeft: 'auto', width: '80%' }}
+                  className='text-white text-right' header="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ؟">
+                  <p className={`m-0 ${myFontIran.className} text-right`}>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز صنعت چاپ، و با استفاده از طراحان گرافیک صنعت چاپ، و با استفاده از طراحان گرافیک لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز صنعت چاپ، و با استفاده.
+                  </p>
+                </AccordionTab>
+                <AccordionTab
+                  style={{ marginRight: 'auto', width: '80%' }}
+                  header="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک؟">
+                  <p className={`m-0 ${myFontIran.className} text-right`}>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز صنعت چاپ، و با استفاده از طراحان گرافیک صنعت چاپ، و با استفاده از طراحان گرافیک لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز صنعت چاپ، و با استفاده.
+                  </p>
+                </AccordionTab>
+                <AccordionTab
+                  style={{ marginLeft: 'auto', width: '80%' }}
+                  header="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک ، چاپگرها و متونن بلکه روزنامه؟">
+                  <p className={`m-0 ${myFontIran.className} text-right`}>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز صنعت چاپ، و با استفاده از طراحان گرافیک صنعت چاپ، و با استفاده از طراحان گرافیک لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز صنعت چاپ، و با استفاده.
+                  </p>
+                </AccordionTab>
+                <AccordionTab
+                  style={{ marginRight: 'auto', width: '80%' }}
+                  header="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک ، چاپگرها و متونن بلکه روزنامه؟">
+                  <p className={`m-0 ${myFontIran.className} text-right`}>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز صنعت چاپ، و با استفاده از طراحان گرافیک صنعت چاپ، و با استفاده از طراحان گرافیک لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز صنعت چاپ، و با استفاده.
+                  </p>
+                </AccordionTab>
+
+              </Accordion>
+            </div>
+
+          </div>
+
         </div>
+
+        <style>
+          {
+            `
+                    .p-accordion .p-accordion-header .p-accordion-header-link {
+                        border: none;
+                        background: #1D1D1D;
+                        color: #fff;
+                        padding: 30px 20px;
+                        font-family: '__myFont_0ebf61';
+                        font-weight:400;
+                    }
+
+                    .p-accordion-header-text {
+                        text-align:right;
+                        line-height:1.8
+                    }
+
+                    .p-accordion-header {
+                        color : white;
+                    }
+
+                    .p-accordion .p-accordion-header:not(.p-disabled).p-highlight .p-accordion-header-link:hover {
+                        background: #1D1D1D;
+                        color : white
+                    }
+
+                    .p-accordion .p-accordion-header:not(.p-disabled).p-highlight .p-accordion-header-link {
+                        border:none;
+                        background : #1D1D1D;
+                        color : white;
+                    }
+
+                    .p-accordion .p-accordion-content {
+                        background : #1D1D1D;
+                        color : white;
+                        border:none
+                    }
+
+                    
+    .p-checkbox .p-checkbox-box {
+        background-color: transparent;
+      }
+  
+      .p-accordion-header-link {
+
+        justify-content: space-between;
+      }
+
+      .p-accordion .p-accordion-header:not(.p-highlight):not(.p-disabled):hover .p-accordion-header-link {
+        background-color: #1D1D1D;
+        border: none;
+        color : white;
+      }
+
+      .p-accordion .p-accordion-header .p-accordion-header-link {
+        height : 70px;
+      }
+
+      .p-accordion .p-accordion-header .p-accordion-header-link .p-accordion-toggle-icon {
+        color : #F68D2E;
+      }
+                    
+                    `
+          }
+        </style>
       </PrimeReactProvider>
     </main>
   )
