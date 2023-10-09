aadc9a1fb089ea644ce27f3e7a15eabe15c0e30b
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-10-09 13:34:37 +0330

    made footer responsive for mobile size

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -21,16 +21,14 @@ const Footer = () => {
     const socialMedia = [facebook, twitter, dribble, pinterest, linkedin]
     return (
         <div className='Footer relative'
-            style={{}}
         >
-            <Image src={footerBg} alt='footerBg' className='w-full lg:h-[600px] h-[500px]' />
 
-            <div className='Footer__content'>
-                <div className={`${myFont.className} Footer__rightside w-3/12 absolute translate-y-2/4`} style={{ right: '6%', bottom: '50%' }}>
+            <div className='Footer__mobile sm:hidden block bg-[#F68D2F]'>
+                <div className={`${myFont.className} Footer__rightside px-12 pt-12`}>
                     <p className='text-white text-right mb-3 lg:text-base text-sm'>
                         همیشه به روز باشید
                     </p>
-                    <p style={{ color: '#1D252D' }} className='text-right text-3xl lg:text-6xl mb-6 leading-normal font-bold'>
+                    <p style={{ color: '#1D252D' }} className='text-right text-3xl lg:text-6xl mb-6 !leading-normal font-bold'>
                         اخبار را از ما دریافت کنید
                     </p>
                     <div className='Footer_rightSide__email flex flex-row-reverse items-center justify-between w-full gap-6'>
@@ -41,14 +39,12 @@ const Footer = () => {
                     </div>
                 </div>
 
-
-                <div className='Footer__center absolute translate-y-2/4 translate-x-2/4 right-[47%] lg:right-1/2 bottom-1/2'
+                <div className='Footer__center'
                 >
-                    <Image src={bull} alt='bull' className='lg:w-fit w-72' />
+                    <Image src={bull} alt='bull' className='mx-auto w-72' />
                 </div>
 
-                <div className={`${myFont.className} Footer__leftside absolute flex flex-col items-end w-4/12 gap-6 translate-y-2/4`}
-                    style={{ left: '1%', bottom: '50%' }}
+                <div className={`${myFont.className} Footer__leftside flex flex-col items-end px-12 pt-6 gap-4`}
                 >
                     <p className='text-white text-right mb-3'>
                         راه های ارتباط با ما
@@ -61,12 +57,11 @@ const Footer = () => {
                 </div>
 
 
-                <div className='absolute w-11/12 left-1/2'
-                    style={{ bottom: '20px', transform: 'translateX(-50%)' }}
+                <div className=''
                 >
-                    <hr className='mb-6 lg:mb-0' />
-                    <div className='flex flex-row-reverse justify-between items-center lg:items-baseline'>
-                        <div className='policies flex flex-row gap-6 items-center'>
+                    <hr className='mb-6 lg:mb-0 mt-12' />
+                    <div className='flex flex-col justify-between items-center lg:items-baseline'>
+                        <div className='policies flex flex-row gap-4 justify-center my-6 items-center flex-wrap'>
                             {policies.map((item) => {
                                 return (
                                     <p className={`${myFontLight.className} text-white font-light`}>
@@ -75,10 +70,6 @@ const Footer = () => {
                                 )
                             })}
                         </div>
-                        <p className={`text-white text-center mt-6 text-xl lg:block hidden ${myFont2.className}`}
-                            style={{ color: '#000' }}
-                        > طراحی شده توسط زفره مدیا </p>
-
                         <div className='flex flex-row gap-6 lg:gap-16'>
                             {socialMedia.map((item) => {
                                 return (
@@ -86,11 +77,83 @@ const Footer = () => {
                                 )
                             })}
                         </div>
+
+                        <p className={`text-white text-center my-6 text-xl ${myFont2.className}`}
+                            style={{ color: '#000' }}
+                        > طراحی شده توسط زفره مدیا </p>
                     </div>
                 </div>
-                
             </div>
 
+            <div className='Footer__default sm:block hidden'>
+                <Image src={footerBg} alt='footerBg' className='w-full lg:h-[600px] h-[500px]' />
+
+                <div className='Footer__content'>
+                    <div className={`${myFont.className} Footer__rightside w-3/12 absolute translate-y-2/4`} style={{ right: '6%', bottom: '50%' }}>
+                        <p className='text-white text-right mb-3 lg:text-base text-sm'>
+                            همیشه به روز باشید
+                        </p>
+                        <p style={{ color: '#1D252D' }} className='text-right text-3xl lg:text-6xl mb-6 !leading-normal font-bold'>
+                            اخبار را از ما دریافت کنید
+                        </p>
+                        <div className='Footer_rightSide__email flex flex-row-reverse items-center justify-between w-full gap-6'>
+                            <input placeholder='ایمیل خود را وارد کنید' className={` outline-none bg-transparent text-black placeholder:text-black pb-1 placeholder:text-right w-full`}
+                                style={{ borderBottom: '3px solid #101010', direction: 'ltr' }}
+                            />
+                            <Image src={arrow} alt='arrow' className='cursor-pointer lg:w-fit w-12' />
+                        </div>
+                    </div>
+
+
+                    <div className='Footer__center absolute translate-y-2/4 translate-x-2/4 right-[47%] lg:right-1/2 bottom-1/2'
+                    >
+                        <Image src={bull} alt='bull' className='lg:w-fit w-72' />
+                    </div>
+
+                    <div className={`${myFont.className} Footer__leftside absolute flex flex-col items-end w-4/12 gap-6 translate-y-2/4`}
+                        style={{ left: '1%', bottom: '50%' }}
+                    >
+                        <p className='text-white text-right mb-3'>
+                            راه های ارتباط با ما
+                        </p>
+                        <FooterInfo title='شماره تماس' info='به زودی' />
+                        <FooterInfo title='ایمیل' info='Arianvest@gmail.com' />
+                        <p className='text-xl lg:text-2xl text-right'>
+                            <span className='font-semibold'> آدرس: </span> مطهری ، میرعماد ، کوچه یازدهم ، پلاک 23 شرکت ، <span className='text-white'> آریان‌وست </span>
+                        </p>
+                    </div>
+
+
+                    <div className='absolute w-11/12 left-1/2'
+                        style={{ bottom: '20px', transform: 'translateX(-50%)' }}
+                    >
+                        <hr className='mb-6 lg:mb-0' />
+                        <div className='flex flex-row-reverse justify-between items-center lg:items-baseline'>
+                            <div className='policies flex flex-row gap-6 items-center'>
+                                {policies.map((item) => {
+                                    return (
+                                        <p className={`${myFontLight.className} text-white font-light`}>
+                                            {item}
+                                        </p>
+                                    )
+                                })}
+                            </div>
+                            <p className={`text-white text-center mt-6 text-xl lg:block hidden ${myFont2.className}`}
+                                style={{ color: '#000' }}
+                            > طراحی شده توسط زفره مدیا </p>
+
+                            <div className='flex flex-row gap-6 lg:gap-16'>
+                                {socialMedia.map((item) => {
+                                    return (
+                                        <Image src={item} alt={item} unoptimized />
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
