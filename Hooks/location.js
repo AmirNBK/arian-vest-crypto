23b978df9f80d5cadc7838c8634094b39531fdbe
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2023-12-02 17:12:49 +0330

    set back to http

--- Hooks/location.js
+++ Hooks/location.js
@@ -8,7 +8,7 @@ const useLocationData = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('https://ip-api.com/json/');
+                const response = await fetch('http://ip-api.com/json/');
 
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
