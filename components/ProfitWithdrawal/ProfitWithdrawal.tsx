ca622459b3da207a5e881d7197e969f1db13e674
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2023-12-21 15:29:11 +0330

    made the ProfitWithdrawal page fully dynamic

--- components/ProfitWithdrawal/ProfitWithdrawal.tsx
+++ components/ProfitWithdrawal/ProfitWithdrawal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import localFont from 'next/font/local'
 import useWindowSize from '@/Hooks/innerSize';
 import Image from 'next/image';
@@ -6,33 +6,66 @@ import certificateMini from '../../assets/icons/certificateMini.svg';
 import StatisticsComponents from '../StatisticsComponents/StatisticsComponents';
 const myFont = localFont({ src: '../../assets/fonts/Mj Dinar Two Medium.ttf' })
 const myFontIran = localFont({ src: '../../assets/fonts/iranyekanwebregular_0.ttf' })
+import { Toast, ToastMessage } from 'primereact/toast';
 import buttonImage from '../../assets/images/profitButton.png'
-import { Wallet } from '@/lib/apiConfig';
+import { Wallet, getProfileInfo, withdrawlRequest } from '@/lib/apiConfig';
 
 const ProfitWithdrawal = (props: {
     isLocationIran: boolean
 }) => {
+    const [userId, setUserId] = useState<number>()
+
+    const [description, setDescription] = useState('');
+
+    const handleTextareaChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        setDescription(event.target.value);
+    };
+    const toastBottomRight = useRef<Toast>(null);
+
 
     interface dataType {
         Pending_withdrawals: number
         amount_withdrawn: number
         number_of_withdrawals: number
         total_profit: number
+        withdrawable_amount: number
     }
 
     const [data, setData] = useState<dataType>()
 
     useEffect(() => {
+        getProfileInfo().then((res) => {
+            setUserId(res.data.pk)
+        })
         Wallet().then((res) => {
             setData(res.data[0])
         })
     }, [])
 
-    console.log(data);
-
+    const handleSendButtonClick = () => {
+        withdrawlRequest(userId, data?.withdrawable_amount, description).then((res) => {
+            if (res.status === 201) {
+                toastBottomRight.current?.show({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: `${props.isLocationIran ? 'درخواست شما با موفقيت ارسال گرديد' : 'Your request has been sent successfully'}`,
+                    life: 3000,
+                });
+            }
+            else {
+                toastBottomRight.current?.show({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: `${props.isLocationIran ? 'ارسال درخواست شما با مشكل مواجه شد' : 'There was a problem sending your request'}`,
+                    life: 3000,
+                });
+            }
+        })
+    }
 
     return (
         <div className='ProfitWithdrawal'>
+            <Toast ref={toastBottomRight} position="bottom-right" />
             <div className='bg-[#1A1C1F] h-full lg:w-full mx-4 lg:mx-6 sm:mx-12 py-8 px-3 sm:px-6 rounded-lg lg:mt-6 mb-10'>
                 <div className='flex flex-col lg:flex-row-reverse gap-2'>
                     <div className='flex flex-row items-center gap-4'>
@@ -90,21 +123,30 @@ const ProfitWithdrawal = (props: {
                     <h3 className={`${myFont.className} text-center mb-2 text-lg text-main-orange`}>
                         مبلغ فعلی
                     </h3>
-                    <StatisticsComponents dollar={true} value={65} isReferral />
+                    <StatisticsComponents dollar={true} value={data?.withdrawable_amount} isReferral />
                 </div>
 
                 <div className='flex-[2] flex flex-col items-center'>
                     <h3 className={`${myFont.className} text-center mb-2 text-lg text-main-orange`}>
                         توضیحات مربوطه خود را بنویسید
                     </h3>
-                    <textarea cols={65} rows={5} className='bg-transparent border border-white rounded-md md:block hidden' />
-                    <textarea cols={35} rows={5} className='bg-transparent border border-white rounded-md md:hidden block' />
+                    <textarea cols={65} rows={5} className={`${props.isLocationIran ? 'rtl' : ''}
+                    bg-transparent border border-white rounded-md md:block hidden text-white`}
+                        value={description}
+                        onChange={handleTextareaChange}
+                    />
+                    <textarea cols={35} rows={5} className={`
+                    ${props.isLocationIran ? 'rtl' : ''}
+                    bg-transparent border border-white rounded-md md:hidden block text-white`}
+                        value={description}
+                        onChange={handleTextareaChange}
+                    />
 
                 </div>
             </div>
 
             <div className='flex justify-center'>
-                <Image src={buttonImage} unoptimized alt='button' />
+                <Image src={buttonImage} unoptimized alt='button' className='cursor-pointer' onClick={handleSendButtonClick} />
             </div>
         </div>
     );
