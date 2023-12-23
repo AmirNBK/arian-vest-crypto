1623fad837a977b8fda33a01423763c2316db151
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2023-12-23 11:25:07 +0330

    made the panel profit withdrawl bilingual

--- components/ProfitWithdrawal/ProfitWithdrawal.tsx
+++ components/ProfitWithdrawal/ProfitWithdrawal.tsx
@@ -8,6 +8,7 @@ import StatisticsComponents from '../StatisticsComponents/StatisticsComponents';
 const myFont = localFont({ src: '../../assets/fonts/Mj Dinar Two Medium.ttf' })
 import empty from '../../assets/icons/empty.png'
 const myFontIran = localFont({ src: '../../assets/fonts/iranyekanwebregular_0.ttf' })
+import profitWithdrawal from '../../assets/images/profitWithdrawal.png'
 import { Toast, ToastMessage } from 'primereact/toast';
 import buttonImage from '../../assets/images/profitButton.png'
 import { Wallet, getProfileInfo, withdrawlRequest } from '@/lib/apiConfig';
@@ -15,6 +16,7 @@ import { Wallet, getProfileInfo, withdrawlRequest } from '@/lib/apiConfig';
 const ProfitWithdrawal = (props: {
     isLocationIran: boolean
 }) => {
+    const isLocationInIran = props.isLocationIran
     const [userId, setUserId] = useState<number>()
     const [loading, setLoading] = useState<boolean>(true)
     const [description, setDescription] = useState('');
@@ -74,86 +76,85 @@ const ProfitWithdrawal = (props: {
                         <>
                             <Toast ref={toastBottomRight} position="bottom-right" />
                             <div className='bg-[#1A1C1F] h-full lg:w-full mx-4 lg:mx-6 sm:mx-12 py-8 px-3 sm:px-6 rounded-lg lg:mt-6 mb-10'>
-                                <div className='flex flex-col lg:flex-row-reverse gap-2'>
-                                    <div className='flex flex-row items-center gap-4'>
-                                        <h2 className={`${myFont.className} Leaderboards__title text-white text-2xl w-fit ml-auto`}>
-                                            برداشت سود
-                                        </h2>
-                                        <Image src={certificateMini} alt='icon' unoptimized />
-                                    </div>
+                                <div className={`flex ${isLocationInIran ? 'lg:flex-row' : 'lg:flex-row-reverse'} items-center gap-4`}>
+                                    <h2 className={`${myFont.className} Leaderboards__title text-white text-2xl w-fit ${isLocationInIran ? 'ml-auto' : 'mr-auto translate-y-0.5'}`}>
+
+                                        {isLocationInIran ? 'برداشت سود' : 'Profit Withdrawal'}
+                                    </h2>
+                                    <Image src={certificateMini} alt='icon' unoptimized />
                                 </div>
 
                                 <div className='flex flex-row-reverse justify-center lg:justify-between mt-8 flex-wrap lg:gap-0 gap-4'>
                                     <div>
                                         <h3 className={`${myFont.className} text-center mb-2 text-lg text-main-orange`}>
-                                            مبلغ برداشت شده
+                                            {isLocationInIran ? 'مبلغ برداشت شده' : 'Withdrawn Amount'}
                                         </h3>
                                         <StatisticsComponents dollar={true} value={data?.amount_withdrawn} isReferral />
                                     </div>
                                     <div>
                                         <h3 className={`${myFont.className} text-center mb-2 text-lg text-main-orange`}>
-                                            برداشت های در انتظار
+                                            {isLocationInIran ? 'برداشت های در انتظار' : 'Pending Withdrawals'}
                                         </h3>
                                         <StatisticsComponents dollar={false} value={data?.Pending_withdrawals} isReferral />
                                     </div>
                                     <div>
                                         <h3 className={`${myFont.className} text-center mb-2 text-lg text-main-orange`}>
-                                            تعداد برداشت ها
+                                            {isLocationInIran ? 'تعداد برداشت ها' : 'Number of Withdrawals'}
                                         </h3>
                                         <StatisticsComponents dollar={false} value={data?.number_of_withdrawals} isReferral />
                                     </div>
                                     <div>
                                         <h3 className={`${myFont.className} text-center mb-2 text-lg text-main-orange`}>
-                                            کل سود
+                                            {isLocationInIran ? 'کل سود' : 'Total Profit'}
                                         </h3>
                                         <StatisticsComponents dollar={true} value={data?.total_profit} isReferral />
                                     </div>
                                 </div>
+
                             </div>
 
-                            <div className='flex flex-col lg:flex-row-reverse mt-8 gap-12 bg-[#1A1C1F] h-full lg:w-full mx-4 lg:mx-6 sm:mx-12 py-8 px-3 sm:px-16 rounded-lg mt-6 mb-20'>
+                            <div className={`flex flex-col  mt-8 gap-12
+                            ${isLocationInIran ? 'lg:flex-row-reverse' : 'lg:flex-row'}
+                            bg-[#1A1C1F] h-full lg:w-full mx-4 lg:mx-6 sm:mx-12 py-8 px-3 sm:px-16 rounded-lg mt-6 mb-20`}>
                                 <div className='flex flex-col flex-1'>
-                                    <div className='flex flex-row items-center gap-4'>
-                                        <h2 className={`${myFont.className} Leaderboards__title text-white text-2xl w-fit ml-auto`}>
-                                            مبلغ قابل برداشت
+                                    <div className={`flex ${isLocationInIran ? 'flex-row' : 'flex-row-reverse'} items-center gap-3`}>
+                                        <h2 className={`${myFont.className} Leaderboards__title text-white text-2xl w-fit ${isLocationInIran ? 'ml-auto' : 'mr-auto translate-y-0.5'}`}>
+                                            {isLocationInIran ? 'مبلغ قابل برداشت' : 'Withdrawable Amount'}
                                         </h2>
                                         <Image src={certificateMini} alt='icon' unoptimized />
                                     </div>
-                                    <p className={`${myFontIran.className} rtl mt-2 text-base`}
-                                        style={{ color: 'rgba(255, 255, 255, 0.40)' }}
-                                    >
-                                        در این قسمت میتوانید مبلغ قابل برداشت خود را مشاهده و در صورت تمایل اقدام به برداشت نمایید تا به حساب شما در بازه زمانی 24 الی 48 ساعت اضافه شود.
+                                    <p className={`${myFontIran.className} ${isLocationInIran && 'rtl'} mt-2 text-base`} style={{ color: 'rgba(255, 255, 255, 0.40)' }}>
+                                        {isLocationInIran
+                                            ? 'در این قسمت میتوانید مبلغ قابل برداشت خود را مشاهده و در صورت تمایل اقدام به برداشت نمایید تا به حساب شما در بازه زمانی 24 الی 48 ساعت اضافه شود.'
+                                            : 'In this section, you can view your withdrawable amount and, if you wish, proceed with the withdrawal process to have it added to your account within 24 to 48 hours.'}
                                     </p>
                                 </div>
 
                                 <div className='flex-1'>
                                     <h3 className={`${myFont.className} text-center mb-2 text-lg text-main-orange`}>
-                                        مبلغ فعلی
+                                        {isLocationInIran ? 'مبلغ فعلی' : 'Current Amount'}
                                     </h3>
                                     <StatisticsComponents dollar={true} value={data?.withdrawable_amount} isReferral />
                                 </div>
 
                                 <div className='flex-[2] flex flex-col items-center'>
                                     <h3 className={`${myFont.className} text-center mb-2 text-lg text-main-orange`}>
-                                        توضیحات مربوطه خود را بنویسید
+                                        {isLocationInIran ? 'توضیحات مربوطه خود را بنویسید' : 'Write your relevant description'}
                                     </h3>
-                                    <textarea cols={65} rows={5} className={`${props.isLocationIran ? 'rtl' : ''}
-                  bg-transparent border border-white rounded-md md:block hidden text-white`}
-                                        value={description}
-                                        onChange={handleTextareaChange}
+                                    <textarea
+                                        cols={65} rows={5} className={`${isLocationInIran ? 'rtl' : ''} bg-transparent border border-white rounded-md md:block hidden text-white`}
+                                        value={description} onChange={handleTextareaChange}
                                     />
-                                    <textarea cols={35} rows={5} className={`
-                  ${props.isLocationIran ? 'rtl' : ''}
-                  bg-transparent border border-white rounded-md md:hidden block text-white`}
-                                        value={description}
-                                        onChange={handleTextareaChange}
+                                    <textarea
+                                        cols={35} rows={5} className={`${isLocationInIran ? 'rtl' : ''} bg-transparent border border-white rounded-md md:hidden block text-white`}
+                                        value={description} onChange={handleTextareaChange}
                                     />
-
                                 </div>
                             </div>
 
+
                             <div className='flex justify-center'>
-                                <Image src={buttonImage} unoptimized alt='button' className='cursor-pointer' onClick={handleSendButtonClick} />
+                                <Image src={isLocationInIran ? buttonImage : profitWithdrawal} unoptimized alt='button' className='cursor-pointer' onClick={handleSendButtonClick} />
                             </div>
                         </>
                         :
@@ -170,7 +171,7 @@ const ProfitWithdrawal = (props: {
                             <div className='flex flex-col justify-center gap-4 items-center'>
                                 <Image src={empty} alt='empty' />
                                 <p className={`${myFontIran.className} text-white ${props.isLocationIran ? 'rtl' : ''}`}>
-                                   در حال حاظر اطلاعات برداشت سود شما موجود نمي باشد. لطفا بعدا امتحان كنيد.
+                                    در حال حاظر اطلاعات برداشت سود شما موجود نمي باشد. لطفا بعدا امتحان كنيد.
                                 </p>
                             </div>
                         </div>
