fbd7c24025a005795831aff65e367cd51a3a4b4c
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2023-12-25 13:37:11 +0330

    adding google analytics to the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
@@ -9,6 +10,21 @@ export default function App({ Component, pageProps }: AppProps) {
   });
   return (
     <ApolloProvider client={client}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-VJB2RDD6QR`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-VJB2RDD6QR', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Component {...pageProps} />
     </ApolloProvider>
   )
